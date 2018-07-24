5f7a5a3c859eb86a761a3343da09eb65e038280e
Ryan Borgstrom <Rkborgstrom@mix.wvu.edu>
2018-07-24 15:47:05 -0600

    updated components

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
-
+import data from '../data.json';
+import Card from './Card';
 
 class Button extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            restaurant: {}
+        }
+    }
+
+    componentDidMount() {
+        this.getRestaurant()
+    }
+
+    getRestaurant = () => {
+        let random = Math.floor(Math.random() * { data }.data.length)
+        let randomRestaurant = { data }.data[random]
+        console.log(randomRestaurant);
+        this.setState({ restaurant: randomRestaurant })
+    }
 
 
     render() {
         return (
-            <button onClick={this.props.getRestaurant}> Random Restaurant </button>
+            <div>
+                <Card restaurant={this.state.restaurant} />
+                <button class="btn btn-info" onClick={this.getRestaurant}> Random Restaurant </button>
+            </div>
         );
     }
 }
